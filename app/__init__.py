e5e5ecc0b92018dc8fbb15b018bb6a99a0c09c53
C107156116 <52107279+C107156116@users.noreply.github.com>
2021-12-27 23:56:35 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -30,32 +30,7 @@ mysql=MySQL(app)
 
 cols=['skin_types','age']
 CORS(app)
-@app.route('/predict_product3',methods=['POST'])
-def  predict_product3():
-    inserValues=request.get_json()
-    productname='青春露'
-    df=pd.read_excel('app/smote/'+productname+'_data_text_process_test_smote.xls')
-    all_cols=['好吸收','透亮','保濕','不引起過敏','不油膩','溫和低刺激','不致痘','不黏膩','修護','春','夏','秋','冬']
-    dic={'混合性肌膚':0,'敏感性肌膚':1,'乾性肌膚':2,'油性肌膚':3,'普通性肌膚':4,'先天過敏性肌膚':5}
-    inserValues['skin_types']=dic[inserValues['skin_types']]
-    process_data=[]
-    for y in range(0,len(cols),1):   
-        max_num=max(df[cols[y]])
-        min_num=min(df[cols[y]])
-        pro_num=round(((float(inserValues[cols[y]])-min_num)/(max_num-min_num)),16)
-        process_data.append(pro_num)
-    for i in all_cols:
-        process_data.append(int(inserValues[i]))
-        print(inserValues[i])
-    pickle_in = open('app/product_predict_model/'+productname+'.pickle','rb')
-    arr=np.array(process_data)
-    print(arr)
-    arr=arr.reshape(1,15)
-    forest = pickle.load(pickle_in)
-    predict_result = forest.predict(arr)
-    
-    return(str(predict_result[0]))
-#    return make_response(dumps(inserValues))
+
 @app.route('/getdata_product1')
 def  getdata():
      tmp=[]
@@ -117,15 +92,8 @@ def  read_productcol():
      inserValues=request.get_json()
      return inserValues
  #------這個路徑應該是沒用到了---------
-@app.route('/getcol_product1')
-def  getcol_product1():
-    inserValues=request.get_json()
-    return inserValues
- #------這個路徑應該是沒用到了---------
-@app.route('/predict_product1',methods=['POST'])
-def  predict_product1():
-    inserValues=request.get_json()
-    effect_cols=['深層清潔','改善粉刺','清潔力好','緊緻毛孔','易沖淨','不引起過敏','溫和低刺激','清爽','控油']
+def predict_method(effect_cols,productname,inserValues):
+#    effect_cols=['深層清潔','改善粉刺','清潔力好','緊緻毛孔','易沖淨','不引起過敏','溫和低刺激','清爽','控油']
     effect_list=[]
     product_effects=(inserValues['product_effects'])
     effect_df=pd.DataFrame(product_effects)
@@ -149,7 +117,7 @@ def  predict_product1():
                 print(season_df[season_col][i])
             except:
                 pass
-    productname='亞馬遜白泥淨緻毛孔面膜'
+#    productname='亞馬遜白泥淨緻毛孔面膜'
     df=pd.read_excel('app/smote/'+productname+'_data_text_process_test_smote.xls')
     dic={'混合性肌膚':0,'敏感性肌膚':1,'乾性肌膚':2,'油性肌膚':3,'普通性肌膚':4,'先天過敏性肌膚':5}
 #    all_cols=['深層清潔','改善粉刺','清潔力好','緊緻毛孔','易沖淨','不引起過敏','溫和低刺激','清爽','控油','春','夏','秋','冬']
@@ -161,13 +129,25 @@ def  predict_product1():
         pro_num=round(((float(inserValues[cols[y]])-min_num)/(max_num-min_num)),16)
         process_data.append(pro_num)
     process_data=process_data+effect_list+season_list
-    print(process_data)
+    print(len(process_data))
     pickle_in = open('app/product_predict_model/'+productname+'.pickle','rb')
     arr=np.array(process_data)
     print(arr)
-    arr=arr.reshape(1,15)
+    arr=arr.reshape(1,len(process_data))
     forest = pickle.load(pickle_in)
     predict_result = forest.predict(arr)
+    return predict_result
+@app.route('/getcol_product1')
+def  getcol_product1():
+    inserValues=request.get_json()
+    return inserValues
+ #------這個路徑應該是沒用到了---------
+@app.route('/predict_product1',methods=['POST'])
+def  predict_product1():
+    inserValues=request.get_json()
+    effect_cols=['深層清潔','改善粉刺','清潔力好','緊緻毛孔','易沖淨','不引起過敏','溫和低刺激','清爽','控油']
+    productname='亞馬遜白泥淨緻毛孔面膜'
+    predict_result=predict_method(effect_cols,productname,inserValues)
     return(str(predict_result[0]))
 #----------------------------------------------    
 @app.route('/predict_product2',methods=['POST'])
@@ -175,100 +155,36 @@ def  predict_product2():
     inserValues=request.get_json()
     productname='激光極淨白淡斑精華'
     df=pd.read_excel('app/smote/'+productname+'_data_text_process_test_smote.xls')
-    dic={'混合性肌膚':0,'敏感性肌膚':1,'乾性肌膚':2,'油性肌膚':3,'普通性肌膚':4,'先天過敏性肌膚':5}
-    all_cols=['透亮','好吸收','保濕','改善暗沉','溫和低刺激','好推勻','用量省','不黏膩','淡化斑點','春','夏','秋','冬']
-    								
-
-    inserValues['skin_types']=dic[inserValues['skin_types']]
-    process_data=[]
-    for y in range(0,len(cols),1):   
-        max_num=max(df[cols[y]])
-        min_num=min(df[cols[y]])
-        pro_num=round(((float(inserValues[cols[y]])-min_num)/(max_num-min_num)),16)
-        process_data.append(pro_num)
-    for i in all_cols:
-        process_data.append(int(inserValues[i]))
-        print(inserValues[i])
-    pickle_in = open('app/product_predict_model/'+productname+'.pickle','rb')
-    arr=np.array(process_data)
-    print(arr)
-    arr=arr.reshape(1,15)
-    forest = pickle.load(pickle_in)
-    predict_result = forest.predict(arr)
-    
+    effect_cols=['透亮','好吸收','保濕','改善暗沉','溫和低刺激','好推勻','用量省','不黏膩','淡化斑點']
+    predict_result=predict_method(effect_cols,productname,inserValues)
+    return(str(predict_result[0]))
+@app.route('/predict_product3',methods=['POST'])
+def  predict_product3():
+    inserValues=request.get_json()
+    productname='青春露'   
+    effect_cols=['好吸收','透亮','保濕','不引起過敏','不油膩','溫和低刺激','不致痘','不黏膩','修護']
+    predict_result=predict_method(effect_cols,productname,inserValues)
     return(str(predict_result[0]))
 @app.route('/predict_product4',methods=['POST'])
 def  predict_product4():
     inserValues=request.get_json()
     productname='R.N.A.超肌能緊緻活膚霜(輕盈版)'
-    df=pd.read_excel('app/smote/'+productname+'_data_text_process_test_smote.xls')
-    dic={'混合性肌膚':0,'敏感性肌膚':1,'乾性肌膚':2,'油性肌膚':3,'普通性肌膚':4,'先天過敏性肌膚':5}
-    all_cols=['保濕','清爽','不黏膩','好推勻','好吸收','延展度佳','修護','不厚重','彈力','輕盈','春','夏','秋','冬']		
-
-    inserValues['skin_types']=dic[inserValues['skin_types']]
-    process_data=[]
-    for y in range(0,len(cols),1):   
-        max_num=max(df[cols[y]])
-        min_num=min(df[cols[y]])
-        pro_num=round(((float(inserValues[cols[y]])-min_num)/(max_num-min_num)),16)
-        process_data.append(pro_num)
-    for i in all_cols:
-        process_data.append(int(inserValues[i]))
-        print(inserValues[i])
-    pickle_in = open('app/product_predict_model/'+productname+'.pickle','rb')
-    arr=np.array(process_data)
-    print(arr)
-    arr=arr.reshape(1,16)
-    forest = pickle.load(pickle_in)
-    predict_result = forest.predict(arr)
+    effect_cols=['保濕','清爽','不黏膩','好推勻','好吸收','延展度佳','修護','不厚重','彈力','輕盈']		
+    predict_result=predict_method(effect_cols,productname,inserValues)
     return(str(predict_result[0]))
 @app.route('/predict_product5',methods=['POST'])
 def  predict_product5():
     inserValues=request.get_json()
     productname='嘉美艷容露'
-    df=pd.read_excel('app/smote/'+productname+'_data_text_process_test_smote.xls')
-    dic={'混合性肌膚':0,'敏感性肌膚':1,'乾性肌膚':2,'油性肌膚':3,'普通性肌膚':4,'先天過敏性肌膚':5}
-    all_cols=['價格實在','清爽','收斂','舒緩','不致痘','鎮定','不黏膩','不油膩','控油','春','夏','秋','冬']		
-    inserValues['skin_types']=dic[inserValues['skin_types']]
-    process_data=[]
-    for y in range(0,len(cols),1):   
-        max_num=max(df[cols[y]])
-        min_num=min(df[cols[y]])
-        pro_num=round(((float(inserValues[cols[y]])-min_num)/(max_num-min_num)),16)
-        process_data.append(pro_num)
-    for i in all_cols:
-        process_data.append(int(inserValues[i]))
-        print(inserValues[i])
-    pickle_in = open('app/product_predict_model/'+productname+'.pickle','rb')
-    arr=np.array(process_data)
-    print(arr)
-    arr=arr.reshape(1,15)
-    forest = pickle.load(pickle_in)
-    predict_result = forest.predict(arr)
+    effect_cols=['價格實在','清爽','收斂','舒緩','不致痘','鎮定','不黏膩','不油膩','控油']		
+    predict_result=predict_method(effect_cols,productname,inserValues)
     return(str(predict_result[0]))
 @app.route('/predict_product6',methods=['POST'])
 def  predict_product6():
     inserValues=request.get_json()
     productname='深層卸粧乳'
-    df=pd.read_excel('app/smote/'+productname+'_data_text_process_test_smote.xls')
-    dic={'混合性肌膚':0,'敏感性肌膚':1,'乾性肌膚':2,'油性肌膚':3,'普通性肌膚':4,'先天過敏性肌膚':5}
-    all_cols=['價格實在','溫和低刺激','易沖淨','清爽','清潔力好','不油膩','不致痘','不緊繃','不引起過敏','春','夏','秋','冬']		
-    inserValues['skin_types']=dic[inserValues['skin_types']]
-    process_data=[]
-    for y in range(0,len(cols),1):   
-        max_num=max(df[cols[y]])
-        min_num=min(df[cols[y]])
-        pro_num=round(((float(inserValues[cols[y]])-min_num)/(max_num-min_num)),16)
-        process_data.append(pro_num)
-    for i in all_cols:
-        process_data.append(int(inserValues[i]))
-        print(inserValues[i])
-    pickle_in = open('app/product_predict_model/'+productname+'.pickle','rb')
-    arr=np.array(process_data)
-    print(arr)
-    arr=arr.reshape(1,15)
-    forest = pickle.load(pickle_in)
-    predict_result = forest.predict(arr)
+    effect_cols=['價格實在','溫和低刺激','易沖淨','清爽','清潔力好','不油膩','不致痘','不緊繃','不引起過敏']		
+    predict_result=predict_method(effect_cols,productname,inserValues)
     return(str(predict_result[0]))
 @app.route('/getdata',methods=['POST'])
 def  getproductdata():
